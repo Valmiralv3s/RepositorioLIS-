0c8dbaa4ef3190c215880adbff9251170f9cf647
Valmir Alves <81195404+Valmiralv3s@users.noreply.github.com>
2021-08-31 23:52:03 -0300

    Update Semana5_Q1.js

--- RepositorioLIS/Semana5_Q1.js
+++ RepositorioLIS/Semana5_Q1.js
@@ -1,1 +1,21 @@
+ function createList(quantItens, mensagem) {
 
+      let i = 0;
+      let ul = document.querySelector("ul");
+
+      while (i < quantItens) {
+
+        // Criar elemento
+        var li = document.createElement('li');
+        var texto = document.createTextNode(mensagem);
+        li.appendChild(texto);
+
+        ul.appendChild(li);
+
+        i++;
+      }
+      // for (let i; i < quantidadeItens; i++){}
+    }
+
+    let nunItens = parseInt(prompt("Quantos itens deseja exibir?"));
+    let mensagem = prompt("Mensagem a ser exibida");
